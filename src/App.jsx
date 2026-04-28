7db996bd24b8a78c85111b332c8e3de0b61dbf0f
siddu-asd <siddueee44@gmail.com>
2026-04-29 00:20:38 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "20px" }}>
+      <h1>React Quiz App</h1>
+
+      <iframe
+        src="https://take.quiz-maker.com/QQ3P3TJDD"
+        width="100%"
+        height="650"
+        title="Quiz"
+        style={{
+          border: "none",
+          borderRadius: "10px",
+          marginTop: "20px"
+        }}
+      ></iframe>
+    </div>
+  );
+}
+
+export default App;
